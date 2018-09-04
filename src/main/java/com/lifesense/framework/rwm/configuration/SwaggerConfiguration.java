7d7f6883feb7d3e86efde4e3a1e44e499fdae395
feilong <weifeilong2013@gmail.com>
2018-09-04 18:00:59 +0800

    update springfox 2.9.2

--- src/main/java/com/lifesense/framework/rwm/configuration/SwaggerConfiguration.java
+++ src/main/java/com/lifesense/framework/rwm/configuration/SwaggerConfiguration.java
@@ -8,12 +8,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @RestController
 @EnableSwagger2
 public class SwaggerConfiguration {
     @Bean
-    public Docket api() {
+    public Docket RedisApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -21,3 +20,4 @@ public class SwaggerConfiguration {
                 .build();
     }
 }
+
