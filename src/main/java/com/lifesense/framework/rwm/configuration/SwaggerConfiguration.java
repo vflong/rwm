8d70b684ecb262837541baed0c7b347af75b7a2f
feilong <weifeilong2013@gmail.com>
2018-09-04 17:12:13 +0800

    alter structure

---
+++ src/main/java/com/lifesense/framework/rwm/configuration/SwaggerConfiguration.java
@@ -0,0 +1,23 @@
+package com.lifesense.framework.rwm.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@RestController
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
--- src/main/java/com/lifesense/framework/rwm/controller/RedisController.java
+++ src/main/java/com/lifesense/framework/rwm/controller/RedisController.java
@@ -1,19 +1,11 @@
 package com.lifesense.framework.rwm.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,21 +13,9 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
-@EnableSwagger2
-@EnableAutoConfiguration
-@EnableGlobalMethodSecurity
 public class RedisController {
     @Value("${spring.redis.host}")
     private String redisHost;
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     String home() {
