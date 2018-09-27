41ec8d9239f9974dd5a49d5346d3efc930e550ec
feilong <weifeilong2013@gmail.com>
2018-09-27 15:09:59 +0800

    redirect / to /swagger-ui.html

--- src/main/java/com/lifesense/framework/rwm/controller/HomeController.java
+++ src/main/java/com/lifesense/framework/rwm/controller/HomeController.java
@@ -5,12 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
-        return "index";
+    public RedirectView home() {
+        return new RedirectView("/swagger-ui.html");
     }
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
